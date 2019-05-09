ca99d383f6b72aa949cce2eab92d0d5fa92458a0
gureuso <wyun13043@gmail.com>
2019-05-09 17:52:39 +0900

    Add mongodb session

--- apps/controllers/test/controllers.py
+++ apps/controllers/test/controllers.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, abort, render_template
-from datetime import datetime
 
 from apps.common.response import ok
-from apps.common.database import db_session
-from apps.models.tests import Test
 
 app = Blueprint('test', __name__, url_prefix='/test')
 
@@ -12,22 +9,6 @@ app = Blueprint('test', __name__, url_prefix='/test')
 @app.route('/ping', methods=['get'])
 def ping():
     return ok('pong')
-
-
-@app.route('/db', methods=['get'])
-def db():
-    now = datetime.now()
-    t = Test(now)
-    db_session.add(t)
-    db_session.commit()
-
-    row = Test.query.filter_by(message=now).first()
-    message = row.message
-
-    Test.query.filter_by(message=now).delete()
-    db_session.commit()
-
-    return ok({'message': message})
 
 
 @app.route('/403', methods=['get'])
