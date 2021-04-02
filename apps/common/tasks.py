4b1d097ebaeb3961da4d6eb27190b50ea743432a
gureuso <wyun13043@gmail.com>
2021-04-03 02:09:57 +0900

    Fix task

--- apps/common/tasks.py
+++ apps/common/tasks.py
@@ -2,8 +2,8 @@ from time import sleep
 from urllib.parse import quote_plus
 from celery import Celery
 
-app = Celery('tasks', broker='sqs://{}:{}@'.format('AKIATE6L2P5UMLPGWLHR',
-                                                   quote_plus('Me1BFxFtoyi8ddZOhfnA+zkN0Ag/D1gja72ZVrH/')))
+app = Celery('tasks', broker='sqs://{}:{}@'.format('ACCESS_KEY',
+                                                   quote_plus('SECRET_KEY')))
 
 
 @app.task
